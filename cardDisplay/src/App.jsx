ef042a4fcf3b1dd5dfc222f3188cb15b6c937083
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-07-20 09:03:02 +0530

    Card Dislay using Api

---
+++ cardDisplay/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  const[cards, setCards] = useState([])
+
+  const fetchDate = async () =>{
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data = await a.json()
+
+    setCards(data)
+    console.log(data)
+  }
+
+  useEffect(() =>{
+    fetchDate()
+  },[])
+
+  return (
+    <>
+    <Navbar/>
+      <div className="container">
+        {cards.map((card) =>{
+          return <div key={card.id} className="card">
+            <h1>{card.title}</h1>
+            <p>{card.body}</p>
+            <span> By UserId: {card.userId}</span>
+            </div>
+
+
+        })}
+
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ cardDisplay/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Nav(){
+    return(
+        <>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Blog</li>
+                    <li>Projects</li>
+                    <li>Contact us</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
